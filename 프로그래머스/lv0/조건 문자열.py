85844c34200030cb6fc25f5a58c89a72fe840dc9
JooHyung <jhp21c@naver.com>
2023-08-30 13:02:47 +0900

    level0

---
+++ 프로그래머스/lv0/조건 문자열.py
@@ -0,0 +1,14 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+
+    if n > m and ineq ==">":
+        answer = 1
+    elif n < m and ineq == "<":
+        answer = 1
+    elif n == m and eq == "=":
+        answer = 1
+
+    return answer
+
+# def solution(ineq, eq, n, m):
+#     return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))
