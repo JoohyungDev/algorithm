ce384f49cea459ec0df0955cf4b790b409ccfbab
JooHyung <jhp21c@naver.com>
2023-09-01 15:28:45 +0900

    level0

--- 프로그래머스/lv0/수열과 구간 쿼리 2.py
+++ 프로그래머스/lv0/수열과 구간 쿼리 2.py
@@ -2,7 +2,7 @@ def solution(arr, queries):
     result = []
     for s,e,k in queries:
         # s부터 e까지의 범위에서 arr을 슬라이싱하고, 그 중 k보다 큰 값들만 골라서 리스트 생성
-        filtered_list = [x for x in arr[s:e+1] if x > k]
+        filtered_list = [i for i in arr[s:e+1] if i > k]
         # 필터링된 리스트가 비어 있지 않으면 가장 작은 값을 결과에 추가
         if filtered_list:
             result.append(min(filtered_list))
---
+++ 프로그래머스/lv0/진료 순서 정하기.py
@@ -0,0 +1,6 @@
+def solution(emergency):
+    answer = []
+    emer_ls = {e: i + 1 for i, e in enumerate(sorted(emergency)[::-1])}
+    for e in emergency:
+        answer.append(emer_ls[e])
+    return answer
