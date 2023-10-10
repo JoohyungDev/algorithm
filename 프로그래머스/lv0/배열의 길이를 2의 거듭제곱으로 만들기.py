a5a132b9f210378ce4fc1e00cd99f944ce6552ad
JooHyung <jhp21c@naver.com>
2023-10-10 12:21:59 +0900

    level0

--- 프로그래머스/lv0/배열의 길이를 2의 거듭제곱으로 만들기.py
+++ 프로그래머스/lv0/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -1,1 +1,11 @@
-asdf
+def solution(arr):
+    length = len(arr)
+    next_power_of_two = 1
+    
+    while next_power_of_two < length:
+        next_power_of_two *= 2
+
+    if length < next_power_of_two:
+        arr.extend([0] * (next_power_of_two - length))
+
+    return arr
