bc7766b14357dc49a4a13bc41cf91503e559dfdb
JooHyung <jhp21c@naver.com>
2023-10-11 15:23:08 +0900

    level0

---
+++ 프로그래머스/lv0/전국 대회 선발 고사.py
@@ -0,0 +1,11 @@
+def solution(rank, attendance):
+    selected = []
+    for i, attend in enumerate(attendance):
+        if attend:
+            selected.append((rank[i], i))
+        print(selected)
+
+    selected.sort()
+    a, b, c = selected[:3]
+
+    return 10000 * a[1] + 100 * b[1] + c[1]
