2f65efb0f041718ff925880f55e26b123c8aa6f5
JooHyung <jhp21c@naver.com>
2023-10-17 12:16:13 +0900

    level0

---
+++ 프로그래머스/lv0/이차원 배열 대각선 순회하기.py
@@ -0,0 +1,8 @@
+def solution(board, k):
+    result = []
+    
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if i+j <= k:
+                result.append(board[i][j])
+    return sum(result)
