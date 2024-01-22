d87a15c9e64f33b27948944ce32ce086829eba2d
JooHyung <jhp21c@naver.com>
2024-01-22 21:07:24 +0900

    파이썬 알고리즘 공부

--- pyalgo100/정렬/평균_점수_카운팅.py
+++ pyalgo100/정렬/평균_점수_카운팅.py
@@ -1,5 +1,12 @@
 # https://100.pyalgo.co.kr/?page=11#
 
+# def solution(data):
+#     filtered_data = list(filter(lambda x:(x[0]+x[1]+x[2])/3>=80, data))
+#     return len(filtered_data)
+
 def solution(data):
-    filtered_data = list(filter(lambda x:(x[0]+x[1]+x[2])/3>=80, data))
+    filtered_data = list(filter(lambda x:x[0]+x[1]+x[2]>240, data))
     return len(filtered_data)
+
+# def solution(data):
+#     return len([x for x in data if sum(x) > 240])
